1cbbd4e7c09bab6291313960f332472835b0b060
JeanOdon <jeanodon1125#git initgit config --global user.name>
2023-08-16 20:48:53 +0200

    Mise à jours

---
+++ script.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+    distance: '45px',
+    duration: 1500,
+    reset: true
+})
+
+sr.reveal('.about,.services,contact', {
+    delay: 200,
+    origin: 'bottom'
+})
+sr.reveal('.about', {
+    delay: 200,
+    origin: 'bottom'
+})
